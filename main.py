fe77c6d0b40a6b828ac4e7d40f9d4d8d321a5774
Winmillion <datum1337@gmail.com>
2020-08-02 23:52:05 +0100

    update

--- main.py
+++ main.py
@@ -15,20 +15,40 @@ async def on_ready():
 async def on_voice_state_update(member: discord.Member, before, after): 
     global FLAG
     
+    role = None
     general_channel = None
+    
+    # gets role
+    for i in member.guild.roles:
+        if i.name == "the last":
+            role = i
+            print(i.members)
+    
     # gets channel
     for i in bot.get_all_channels():
         if i.name == "gen":
             general_channel = i
         
+    # sets flag if people in channel equates greater than set ammount
     if after.channel != None and len(after.channel.members) >= 1 and FLAG == False:
         FLAG = True
         print(f"Flag set: {FLAG}")
+        
+    # if last to leave
     if after.channel == None and FLAG == True:
         if len(before.channel.members) == 0:
             print(member.name)  
             FLAG = False
-            await general_channel.send("I am responding to your message")  
+            await general_channel.send(f"<@{member.id}> youre last :P")
+            # removes role from everyone 
+            for m in member.guild.members:
+                try:
+                    await m.remove_roles(role)
+                except:
+                    print("Couldnt remove role")
+            # sets role to last to leave
+            await bot.set_roles(member, role)
+                
 
 if __name__ == "__main__":
     bot.run(TOKEN)
