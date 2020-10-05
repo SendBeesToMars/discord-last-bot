fc1baf7da0fdeed1d49c70061063169aafd6f7b7
Winmillion <datum1337@gmail.com>
2020-10-05 23:56:55 +0100

    @'s a person if he joins and leaves the vc too quickly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import discord
 import os
+import time
 
 TOKEN = os.environ.get('DOOTDOOT_TOKEN')
 FLAG = False
@@ -12,32 +13,51 @@ async def on_ready():
     print("Bot is ready")
 
 @bot.event
-async def on_voice_state_update(member: discord.Member, before, after): 
+async def on_voice_state_update(member: discord.Member, before, after):
+    """ sets role to last person to leave voice channel.
+        Removes role from previous role holders. 
+        Sends a message in chat, with last person tagged """
     global FLAG
     
+    FLAG_PLYER_NUM = 3
+    
     # gets role
-    role =              discord.utils.get(member.guild.roles, name="the last")
+    role =              discord.utils.get(member.guild.roles, name="test role")
     
     # gets channel
-    general_channel =   discord.utils.get(bot.get_all_channels(), name="gen")
+    general_channel =   discord.utils.get(bot.get_all_channels(), name="bot")
         
     # sets flag if people in channel equates greater than set ammount
-    if after.channel != None and len(after.channel.members) >= 3 and FLAG == False:
+    if after.channel != None and len(after.channel.members) >= FLAG_PLYER_NUM and FLAG == False:
+        flag_timer = time.time() # time object
         FLAG = True
         print(f"Flag set: {FLAG}")
+        
+    # @'s a person if he joins and leaves the vc too quickly
+    if after.channel != None and len(after.channel.members) >= FLAG_PLYER_NUM-1 and FLAG == True:
+        if time.time() - flag_timer <= 5:
+            FLAG = False
+            print(member.name)
+            await general_channel.send(f"<@{member.id}> suh")
+            # removes role from everyone 
+            for i in role.members:
+                await i.remove_roles(role)
+            # sets role to last to leave
+            await member.add_roles(role)            
         
     # if last to leave
     if after.channel == None and FLAG == True:
         if len(before.channel.members) == 0:
             print(member.name)  
             FLAG = False
-            await general_channel.send(f"<@{member.id}> hehe")
+            await general_channel.send(f"<@{member.id}> suh")
             # removes role from everyone 
-            for member in role.members:
-                await member.remove_roles(role)
+            for i in role.members:
+                await i.remove_roles(role)
             # sets role to last to leave
             await member.add_roles(role)
                 
 
 if __name__ == "__main__":
     bot.run(TOKEN)
+    
